754b8bb7799ff25b8435e1d08088bf13713068a0
Rohan Nigam <rohan.nigam@stern.nyu.edu>
2017-07-24 20:18:31 -0400

    Checkpoint III Completed

--- app/products_app.py
+++ app/products_app.py
@@ -2,26 +2,34 @@ import csv
 
 products = []
 
-csv_file_path = "data/products.csv"
+products_csv = "data/products.csv"
 
-# READ PRODUCTS CSV
-with open(csv_file_path, "r") as csv_file:
+headers = ["id", "name", "aisle", "department", "price"] # for "Further Exploration" use: ["product_id", "product_name", "aisle_id", "aisle_name", "department_id", "department_name", "price"]
+user_input_headers = [header for header in headers if header != "id"] # don't prompt the user for the product_id
+
+def get_product_id(product): return int(product["id"])
+
+def auto_incremented_id():
+    product_ids = map(get_product_id, products)
+    return max(product_ids) + 1
+
+#
+# READ PRODUCTS FROM FILE
+#
+
+with open(products_csv, "r") as csv_file:
     reader = csv.DictReader(csv_file)
-    for row in reader:
-        products.append(row)
-menu = """
-    Hi.
-    Welcome to the products app.
-    There are {0} products.
-    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
-    Please choose an operation:
-""".format(len(products))
-chosen_operation = input(menu)
-chosen_operation = chosen_operation.title()
+    for ordered_dict in reader:
+        products.append(dict(ordered_dict))
+
+#
+# HANDLE USER INPUT
+#
+
 def list_products():
-    print("LISTING PRODUCTS")
+    print("LISTING PRODUCTS HERE")
     for product in products:
-       print(" + Product #" + str(product["id"]) + ": " + product["name"])
+        print(" + Product #" + str(product["id"]) + ": " + product["name"])
 
 def show_product():
     product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
@@ -30,34 +38,72 @@ def show_product():
         print("READING PRODUCT HERE", product)
     else:
         print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product)
+
 def create_product():
-    print("CREATING A PRODUCT")
-    product_name = input("name is:")
-    product_aisle = input("aisle is:")
-    product_department = input("department is:")
-    product_price = input("price is:")
-    new_product = {
-        "id": len(products) + 1,
-        "name": product_name,
-        "aisle": product_aisle,
-        "department": product_department,
-        "price": product_price
-    }
-    print("NEW PRODUCT IS", new_product)
-    products.append(new_product)
+    print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
+    product = {"id": auto_incremented_id() }
+    for header in user_input_headers:
+        product[header] = input("The '{0}' is: ".format(header))
+    products.append(product)
+    print("CREATING PRODUCT HERE", product)
+
 def update_product():
-    print("UPDATING A PRODUCT")
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("OK. PLEASE PROVIDE THE PRODUCT'S INFORMATION...")
+        for header in user_input_headers:
+            product[header] = input("Change '{0}' from '{1}' to: ".format(header, product[header]))
+        print("UPDATING PRODUCT HERE", product)
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
+
 def destroy_product():
-    print("DESTROYING A PRODUCT")
-if chosen_operation == "List": list_products()
-elif chosen_operation == "Show": show_product()
-elif chosen_operation == "Create": create_product()
-elif chosen_operation == "Update": update_product()
-elif chosen_operation == "Destroy": destroy_product()
-else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
-# OVERWRITING INVENTORY CSV FILE
-with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
-    writer.writeheader() # uses fieldnames set above
+    product_id = input("OK. WHAT IS THE PRODUCT'S ID? ")
+    product = [p for p in products if p["id"] == product_id][0]
+    if product:
+        print("DESTROYING PRODUCT HERE", product)
+        del products[products.index(product)]
+    else:
+        print("COULDN'T FIND A PRODUCT WITH IDENTIFIER", product_id)
+
+menu = """
+-----------------------------------
+PRODUCTS APPLICATION
+-----------------------------------
+Welcome {0}!
+There are {1} products in the database.
+    operation | description
+    --------- | ------------------
+    'List'    | Display a list of product identifiers and names.
+    'Show'    | Show information about a product.
+    'Create'  | Add a new product.
+    'Update'  | Edit an existing product.
+    'Destroy' | Delete an existing product.
+Please select an operation: """.format("@s2t2", len(products)) # end of multi- line string. also using string interpolation
+
+crud_operation = input(menu)
+
+if crud_operation.title() == "List":
+    list_products()
+elif crud_operation.title() == "Show":
+    show_product()
+elif crud_operation.title() == "Create":
+    create_product()
+elif crud_operation.title() == "Update":
+    update_product()
+elif crud_operation.title() == "Destroy":
+    destroy_product()
+else:
+    print("OOPS SORRY. PLEASE TRY AGAIN.")
+
+#
+# WRITE PRODUCTS TO FILE
+#
+
+with open(products_csv, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=headers)
+    writer.writeheader()
+
     for product in products:
         writer.writerow(product)
